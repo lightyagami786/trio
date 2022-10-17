5174fc21c361047bf6d64b3ed211db3e0ffbedc3
lightyagami786 <109030390+lightyagami786@users.noreply.github.com>
2022-10-17 20:22:45 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  
+  const[joke,setJoke]=useState('')
+
+  let newJoke=()=>{
+
+    fetch("https://v2.jokeapi.dev/joke/Any?type=single").then(res=>res.json()).then(res2=>{
+      setJoke(res2.joke)
+    })
+
+  }
+
+  useEffect(
+    ()=>{
+      newJoke();
+      
+    },[]);
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+
+    <div><h1>&nbsp;&nbsp;&nbsp;joke of the day</h1></div>
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{joke}
+    <br/>
+    <br/>
+    <button onClick={newJoke} >click for more</button>    
+    </>
   );
 }
 
